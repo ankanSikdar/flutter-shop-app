c9202ea28903bed621fad63a46415c5a03dff1df
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-03 19:46:18 +0530

    Wrapped GridTile with ClipRRect

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,34 +9,37 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          icon: Icon(
-            Icons.favorite,
-            color: Theme.of(context).iconTheme.color,
-          ),
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
-        title: Container(
-          width: 80,
-          child: Text(
-            title,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.headline6,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () {},
           ),
-        ),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.add_shopping_cart,
-            color: Theme.of(context).iconTheme.color,
+          title: Container(
+            width: 80,
+            child: Text(
+              title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline6,
+            ),
           ),
-          onPressed: () {},
+          trailing: IconButton(
+            icon: Icon(
+              Icons.add_shopping_cart,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            onPressed: () {},
+          ),
         ),
       ),
     );
