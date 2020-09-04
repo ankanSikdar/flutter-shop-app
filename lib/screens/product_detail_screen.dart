e78ae27dde81b9000d4034684cd5c10ada273c7c
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-04 18:02:35 +0530

    Added Provider to Product Detail Screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail-screen';
@@ -6,10 +8,14 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final item = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
-        title: productId == null ? Text('Title') : Text(productId),
+        title: Text(item.title),
       ),
     );
   }
