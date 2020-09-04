ebf0d4969d44d1b0dae2399e7c27a3a7578b080e
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-04 17:42:12 +0530

    Set debugShowCheckedModeBanner to false

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => Products(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.orange,
