20f6ef87b5c957917c2b53cadc26724aa7f3b9cf
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-04 17:00:44 +0530

    Added and implemented the Provider Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
@@ -9,26 +11,29 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.orange,
-        fontFamily: 'Rajdhani',
-        textTheme: TextTheme(
-          headline6: TextStyle(
-            fontFamily: 'Rajdhani',
-            fontSize: 18,
-            color: Colors.white,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.orange,
+          fontFamily: 'Rajdhani',
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              fontFamily: 'Rajdhani',
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Colors.orange,
           ),
         ),
-        iconTheme: IconThemeData(
-          color: Colors.orange,
-        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:shop_app/models/product.dart';
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/widgets/product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _productsData = Provider.of<Products>(context);
+    final List<Product> _products = _productsData.items;
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: 1 / 1,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      itemBuilder: (context, index) {
+        return ProductItem(
+          id: _products[index].id,
+          title: _products[index].title,
+          imageUrl: _products[index].imageUrl,
+        );
+      },
+      itemCount: _products.length,
+      padding: EdgeInsets.all(20),
+    );
+  }
+}
