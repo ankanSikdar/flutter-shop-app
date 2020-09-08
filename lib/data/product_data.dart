7ce13597c1ba9dfd98d67b6a65b9121611bac8b1
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-08 17:16:22 +0530

    Moved data to a new file

---
+++ lib/data/product_data.dart
@@ -0,0 +1,95 @@
+import 'package:shop_app/providers/product.dart';
+
+List<Product> data = [
+  Product(
+    id: 'p1',
+    title: 'Red Shirt',
+    description: 'A red shirt - it is pretty red!',
+    price: 999.99,
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+  ),
+  Product(
+    id: 'p2',
+    title: 'Trousers',
+    description: 'A nice pair of trousers.',
+    price: 1559.99,
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+  ),
+  Product(
+    id: 'p3',
+    title: 'Yellow Scarf',
+    description: 'Warm and cozy - exactly what you need for the winter.',
+    price: 599.99,
+    imageUrl: 'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
+  ),
+  Product(
+    id: 'p4',
+    title: 'A Pan',
+    description: 'Prepare any meal you want.',
+    price: 499.99,
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+  ),
+  Product(
+    id: 'p5',
+    title: 'TV Entertainment Unit',
+    description: 'Home Edge Sheesham Wood Solid Wood TV Entertainment Unit',
+    price: 24490.00,
+    imageUrl:
+        'https://rukminim1.flixcart.com/image/416/416/jr83gy80/tv-entertainment-unit/y/c/6/rosewood-sheesham-weaveloungetvunit-1-home-edge-teak-color-original-imafd2d3h77jfbyh.jpeg',
+  ),
+  Product(
+    id: 'p6',
+    title: 'N95 Masks',
+    description: 'Keep Yourself Safe from COVID-19',
+    price: 599.99,
+    imageUrl:
+        'https://rukminim1.flixcart.com/image/416/416/kdt50nk0/mask-respirator/p/u/s/l-2-mask-1007-blue-cloth-mask-asian-original-imafumgxgwuhacqk.jpeg',
+  ),
+  Product(
+    id: 'p7',
+    title: 'Flip Flops',
+    description: 'Keep Your feet warm',
+    price: 899.99,
+    imageUrl:
+        'https://rukminim1.flixcart.com/image/880/1056/k5wse4w0/slipper-flip-flop/h/g/z/eezay-maxout-grey-6-adidas-grey-original-imafzhhsmyzc8fdy.jpeg',
+  ),
+  Product(
+    id: 'p8',
+    title: 'Blue KGB Polo',
+    description:
+        'Shop from a wide range of Polos from KGB. Perfect for your everyday use, you could pair it with a stylish pair of Jeans or Trousers complete the look.',
+    price: 499.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/51XgLfhoZoL._UL1329_.jpg',
+  ),
+  Product(
+    id: 'p9',
+    title: 'AMERICAN CREW Men\'s Polo AC160 Grey',
+    description:
+        'Regular Fit Half Sleeve Polo is made of comfortable, Bio Washed cotton-poly pique fabric, a three-button placket, and ribbed cuffs for a classic look. Fabric Composition - Cotton 60% Poly 40% Blend, Bio Wash Pique Fabric. Pattern - Color Block Men’s Polo Tshirt, Rib Collar & Sleeve for Amazing Fit. Classic "American Crew" Signature Polo with Embroidery on Chest. “Made In India” by Socially Compliant MSME Factory. All Components Used to make this T-Shirt are Proudly "Made in India".',
+    price: 599.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/71XUKEYszsL._UL1500_.jpg',
+  ),
+  Product(
+    id: 'p10',
+    title: 'Womanista Women\'s Georgette with Blouse Piece',
+    description:
+        'Black party wear poly georgette saree has floral prints on it. This latest fancy designer piece comes with unstitched solid blouse and embellished mirror work lace. Grab this piece for a smart party look.',
+    price: 1299.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/81XKaSKvlyL._UL1500_.jpg',
+  ),
+  Product(
+    id: 'p11',
+    title: 'ANNI DESIGNER Women\'s Crepe Printed Salwer Suit',
+    description:
+        'A salwar suit is a combination of tunic or Kameez and a pair of trousers or salwar, usually accompanied by a scarf or Dupatta. Salwar suits are available in a wide range of designs and are more tight-fitting and modern in design when compared to the traditional kameez.',
+    price: 1599.99,
+    imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/812J5EKN6GL._UL1500_.jpg',
+  ),
+];
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,41 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/providers/product.dart';
+import 'package:shop_app/data/product_data.dart';
 
 class Products with ChangeNotifier {
-  List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 999.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 1559.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Yellow Scarf',
-      description: 'Warm and cozy - exactly what you need for the winter.',
-      price: 599.99,
-      imageUrl:
-          'https://live.staticflickr.com/4043/4438260868_cc79b3369d_z.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 499.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
-  ];
+  List<Product> _items = data;
 
   List<Product> get items {
     return [..._items];
