99317b0a7f6914d68233380eb155c301ba857b57
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-09 18:19:19 +0530

    Added User Products Screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                UserProductItem(
+                  title: productsData.items[index].title,
+                  imageUrl: productsData.items[index].imageUrl,
+                ),
+                Divider(),
+              ],
+            );
+          },
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
-import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/providers/products.dart';
 
 class CartItem extends StatelessWidget {
