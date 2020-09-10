b5708c02c765e71246109f18fc3ec2a013ec0cf9
Ankan Sikdar <ankansikdar@gmail.com>
2020-09-10 15:58:56 +0530

    Added ability to delete a product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -41,4 +41,9 @@ class Products with ChangeNotifier {
     );
     notifyListeners();
   }
+
+  void removeProduct(String id) {
+    _items.removeWhere((element) => element.id == id);
+    notifyListeners();
+  }
 }
--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
@@ -34,7 +36,41 @@ class UserProductItem extends StatelessWidget {
                 Icons.delete,
                 color: Theme.of(context).errorColor,
               ),
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                        'Are you Sure?',
+                        style: Theme.of(context).textTheme.headline6.copyWith(
+                              fontSize: 25,
+                              color: Colors.black,
+                            ),
+                      ),
+                      content: Text(
+                          'You want to delete $title from your products ?'),
+                      actions: [
+                        FlatButton(
+                          child: Text('Yes'),
+                          onPressed: () {
+                            Provider.of<Products>(context, listen: false)
+                                .removeProduct(id);
+                            Navigator.pop(context);
+                          },
+                        ),
+                        FlatButton(
+                          child: Text('No'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
             )
           ],
         ),
